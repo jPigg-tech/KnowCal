c7b5cb5291215aa98929f589d444bf2b3942f103
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-11 18:13:59 -0500

    Created a new model class SearchedFood and gave it some properties

--- CalorieTracker/Models/SearchedFood.cs
+++ CalorieTracker/Models/SearchedFood.cs
@@ -10,5 +10,9 @@ namespace CalorieTracker.Models
     {
         [Key]
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string  BrandName { get; set; }
+        public string  Calories { get; set; }
+        public int ServingSize { get; set; }
     }
 }
