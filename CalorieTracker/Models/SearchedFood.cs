0771630ae2748130aa69c02f4a4d991f0863d4e6
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-12 20:11:13 -0500

    Added display names in the SearchedFood Model

--- CalorieTracker/Models/SearchedFood.cs
+++ CalorieTracker/Models/SearchedFood.cs
@@ -10,9 +10,17 @@ namespace CalorieTracker.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Food Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Brand Name")]
         public string  BrandName { get; set; }
+
+        [Display(Name = "Calorie Amount")]
         public int  Calories { get; set; }
+
+        [Display(Name = "Serving Size")]
         public int ServingSize { get; set; }
     }
 }
